17e7aef9874ce3c39a7f59cd72ec6977c20132c8
Janhvi Tiwari <janhvit611@gmail.com>
2026-08-14 11:53:22 +0530

    initial spring boot project with mongodb

---
+++ src/main/java/com/SIH/Women/Safety/Device/WomenSafetyDeviceApplication.java
@@ -0,0 +1,13 @@
+package com.SIH.Women.Safety.Device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomenSafetyDeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomenSafetyDeviceApplication.class, args);
+	}
+
+}
